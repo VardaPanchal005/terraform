3a42f96952b8e2d3324211160d196195cb4be6c7
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2025-01-22 15:02:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "webSg" {
 
 
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-00bb6a80f01f03502"
+  ami                    = "your ami id"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
@@ -85,7 +85,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-00bb6a80f01f03502"
+  ami                    = "your ami id"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub2.id
