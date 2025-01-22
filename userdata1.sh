9a1e5f225d3196504b9560f34939a7506236e270
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2025-01-22 10:56:28 +0530

    Add files via upload

---
+++ userdata1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+apt update
+apt install -y apache2
+
+# Fetch the session token (required for IMDSv2)
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+# Get the instance ID using the instance metadata and session token
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+
+# Install the AWS CLI
+apt install -y awscli
+
+# Create a favicon (optional)
+touch /var/www/html/favicon.ico  # Or download your own
+
+# Create a simple HTML file with the portfolio content and display the images
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <style>
+    /* Add animation and styling for the text */
+    @keyframes colorChange {
+      0% { color: red; }
+      50% { color: green; }
+      100% { color: blue; }
+    }
+    h1 {
+      animation: colorChange 2s infinite;
+    }
+  </style>
+</head>
+<body>
+  <h1>Terraform Project Server 1</h1>
+  <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
+  <p><p>Welcome to Instance 2</p></p>
+</body>
+</html>
+EOF
+
+# Start Apache and enable it on boot
+systemctl start apache2
+systemctl enable apache2
